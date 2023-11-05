9def6aa8c39fdbe4cc64f451c90687dc47470daf
ali <ali.raee@aut.ac.ir>
2023-11-06 01:34:48 +0330

    feat:add baceknd and frontend

---
+++ welcome_page/client/src/components/Footer.jsx
@@ -0,0 +1,16 @@
+import '../index.css'
+function Footer() {
+    return (
+
+        <footer class="bg-gray-800 rounded-lg shadow m-4">
+            <div class="w-full mx-auto max-w-screen-xl p-4 ">
+                <span class="flex justify-center text-gray-100">made in with ❤️ in IRAN.
+                </span>
+
+            </div>
+        </footer>
+
+    )
+}
+
+export default Footer
